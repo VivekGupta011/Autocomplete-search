49193ac96b4d783845e636a371565e83db9f9f14
vivek gupta <viveksite01@gmail.com>
2024-01-16 00:37:47 +0530

    feature added

---
+++ src/app/(website)/page.js
@@ -0,0 +1,11 @@
+import Example from "./Example";
+export default async function Home() {
+
+  return (
+    <main>
+      <section className="pt-32">
+          <Example/>
+      </section>
+    </main>
+  )
+}
---
+++ src/components/data.js
@@ -0,0 +1,128 @@
+// Manually added dummy data
+export  const manualDummyData = [
+    {
+      profileImage: 'https://picsum.photos/30/30?random=10',
+      name: 'Aarav Sharma',
+      username: 'aarav_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=11',
+      name: 'Aanya Patel',
+      username: 'aanya_patel',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=12',
+      name: 'Arjun Gupta',
+      username: 'arjun_gupta',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=13',
+      name: 'Aditi Verma',
+      username: 'aditi_verma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=14',
+      name: 'Aryan Singh',
+      username: 'aryan_singh',
+    },
+  
+    {
+      profileImage: 'https://picsum.photos/30/30?random=15',
+      name: 'Zara Khan',
+      username: 'zara_khan',
+    },{
+      profileImage: 'https://picsum.photos/30/30?random=16',
+      name: 'Bhavya Sharma',
+      username: 'bhavya_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=17',
+      name: 'Chetan Patel',
+      username: 'chetan_patel',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=18',
+      name: 'Deepa Gupta',
+      username: 'deepa_gupta',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=19',
+      name: 'Esha Verma',
+      username: 'esha_verma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=20',
+      name: 'Fahad Singh',
+      username: 'fahad_singh',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=22',
+      name: 'Gauri Khan',
+      username: 'gauri_khan',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=24',
+      name: 'Hitesh Sharma',
+      username: 'hitesh_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=25',
+      name: 'Isha Patel',
+      username: 'isha_patel',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=26',
+      name: 'Jatin Gupta',
+      username: 'jatin_gupta',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=28',
+      name: 'Lavanya Sharma',
+      username: 'lavanya_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=1',
+      name: 'Jatin Sharma',
+      username: 'jatin_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=2',
+      name: 'Kavya Patel',
+      username: 'kavya_patel',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=3',
+      name: 'Lakshay Gupta',
+      username: 'lakshay_gupta',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=4',
+      name: 'Maya Verma',
+      username: 'maya_verma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=5',
+      name: 'Nikhil Singh',
+      username: 'nikhil_singh',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=6',
+      name: 'Vidya Khan',
+      username: 'vidya_khan',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=7',
+      name: 'Gaurav Sharma',
+      username: 'gaurav_sharma',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=8',
+      name: 'Deepika Patel',
+      username: 'deepika_patel',
+    },
+    {
+      profileImage: 'https://picsum.photos/30/30?random=9',
+      name: 'Sanjay Gupta',
+      username: 'sanjay_gupta',
+    },
+  ];
